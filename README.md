c64eb924b15c2bce4823a75f3560976475ba6197
James R <Reedcj798@gmail.com>
2023-02-11 10:18:03 -0700

    Added comments for clarity, added some items to md

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 An exploration of Java programming and Java syntax
 
-Created a basic Tic Tac Toe game to demonstrate and explore simple game programming in a Java environment
+Created a basic Tic-Tac-Toe game to demonstrate and explore simple game programming in a Java environment
 
 Mostly for my own beinifit to familiarize myself with java, though others are free to reference the material for their own learning
 
@@ -29,3 +29,4 @@ Additioal libraries included:
 - Improved UI - current ui is poor and could be vastly improved
 - More consise code - current code is potentialy to wordy and clunky could be streamlined
 - Consider using an array rather thatn a basic list - list causes problems while coding and makes it harder to program effectively without constantly referencing the printBoard method
+- potentially expand game to 3d Tic-Tac-Toe (likely requires complete overhaul)
--- tictactoe.java
+++ tictactoe.java
@@ -11,15 +11,18 @@ public class tictactoe {
                          "  -------------\n");
     }
 
+    // Method for placing either an X or an O on the board in designated position
     void placePiece(String p,String board[]){
-        Scanner myObj = new Scanner(System.in);
+        Scanner myObj = new Scanner(System.in); // Scanner for recieving inputs
         System.out.print(p + ", please input tile choice(a1,a2,b3...etc.):\n");
-        String place = myObj.nextLine();
-        place = place.toLowerCase().replaceAll("\\s", "");
+        String place = myObj.nextLine(); // Grabs string
+        place = place.toLowerCase().replaceAll("\\s", ""); // forces lowercase and removes spaces for code integrity and simplicity
+        // NOTE: test code kept for future development
         // System.out.print(place);
         // if (place.equals("a1")){
         //     System.out.print("a1");
         // }
+        // tests space and if its empty then places apropriate player piece
         if (place.equals("a1") & board[0].equals(" ")){
             board[0] = p;
         }else if (place.equals("b1") & board[1].equals(" ")){
@@ -38,17 +41,16 @@ public class tictactoe {
             board[7] = p;
         }else if (place.equals("c3") & board[8].equals(" ")){
             board[8] = p;
-        }else if (place.equals("a1")||place.equals("b1")||place.equals("c1")||place.equals("a2")||place.equals("b2")||place.equals("c2")||place.equals("a3")||place.equals("b3")||place.equals("c3")){
+        }else if (place.equals("a1")||place.equals("b1")||place.equals("c1")||place.equals("a2")||place.equals("b2")||place.equals("c2")||place.equals("a3")||place.equals("b3")||place.equals("c3")){ // tests f input was valid but not empty
             System.out.print("Please choose a valid EMPTY space\n");
-            printBoard(board);
-            placePiece(p, board);
-        }else{
+            gameRound(p, board); // Reruns without changing player
+        }else{ // Prints standard error for incorrect input
             System.out.print("Please input a valid space code(i.e. a1, a2, b3...etc)\n");
-            printBoard(board);
-            placePiece(p, board);
+            gameRound(p, board); // Reruns without changing players
         }
     }
     
+    // Tests all possible win conditions
     static boolean winTest(String p, String board[]){
         if (board[0].equals(" ") == false & board[0].equals(board[1]) & board[0].equals(board[2])){
             return false;
@@ -68,29 +70,30 @@ public class tictactoe {
         return true;
     }
 
+    // underutilizd method for current development though could be more useful in future itterations and versions
     void gameRound(String p,String board[]){
         printBoard(board);
         placePiece(p, board);
     }
 
     public static void main(String args[]){
-        String[] board = {" "," "," "," "," "," "," "," "," "};
-        boolean game = true;
-        int rounds = 0;
-        String p = "X"; 
-        tictactoe game_1 = new tictactoe();
+        String[] board = {" "," "," "," "," "," "," "," "," "}; // all board spaces need to maintain certain spacing
+        boolean game = true; // game is running tester
+        int rounds = 0; // chescker for if the game has reeched maximum possible moves
+        String p = "X"; // establishes player
+        tictactoe game_1 = new tictactoe(); // innitialize game
         while (game){
             game_1.gameRound(p,board);
             rounds += 1;
             game = winTest(p,board);
-            if (rounds == 9){
+            if (rounds == 9){ // Tie testing
                 game = false;
                 game_1.printBoard(board);
                 System.out.print("Game over! You tie!\n");
-            }else if (game == false){
+            }else if (game == false){ // win message
                 game_1.printBoard(board);
                 System.out.print("Game over! " + p + " wins!\n");
-            }else if (p.equals("X")){
+            }else if (p.equals("X")){ // shifts player
                 p = "O";
             }else if (p.equals("O")){
                 p = "X";
